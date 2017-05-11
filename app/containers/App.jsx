1d92ea7c1d5ebc38ebda28770ca6b811fa8d5fa4
wuyang <wuyang@cloudin.cn>
2017-05-11 19:26:20 +0800

    增加redux使用

--- app/containers/App.jsx
+++ app/containers/App.jsx
@@ -1,8 +1,21 @@
 import React from "react"
+
+import { Provider } from 'react-redux'
+import configureStore from '../store/configureStore'
+
+import UserInfo from './UserInfo'
+
+const store = configureStore()
+
 class App extends React.Component{
     render(){
         return (
-            <div>{this.props.children}</div>
+            <div>
+                <Provider store = {store}>
+                    <UserInfo />
+                </Provider>
+                {this.props.children}
+            </div>
         )
     }
 }
---
+++ app/containers/UserInfo.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+import * as userinfoActions from '../actions/userinfo'
+
+class UserInfo extends React.Component{
+    render(){
+        return (
+            <div>
+                <p>
+                    ID：{this.props.userinfo.userid}
+                </p>       
+                <p>
+                    城市：{this.props.userinfo.city}
+                </p>       
+            </div>
+        )
+    }
+    componentDidMount(){
+        this.props.userinfoActions.login({
+            userid: 'abc',
+            city: 'beijing'
+        })
+    }
+}
+
+function mapStateToProps(state){
+    return {
+        userinfo: state.userinfo
+    }
+}
+
+function mapDispatchToProps(dispatch){
+    return {
+        userinfoActions: bindActionCreators(userinfoActions, dispatch)
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(UserInfo)
